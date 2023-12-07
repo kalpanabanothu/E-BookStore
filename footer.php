4f6d1502477138bca6079e2f1ad9a0e42680f263
kalpanabanothu <136302629+kalpanabanothu@users.noreply.github.com>
2023-12-07 22:11:50 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,31 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php">home</a>
+         <a href="about.php">about</a>
+         <a href="shop.php">shop</a>
+         <a href="contact.php">contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="login.php">login</a>
+         <a href="register.php">register</a>
+         <a href="cart.php">cart</a>
+         <a href="orders.php">orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +91 9492824215 </p>
+         <p> <i class="fas fa-phone"></i> +91 9347060864 </p>
+         <p> <i class="fas fa-envelope"></i> kalpanabanothub@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Nirmal, india - 504107  </p>
+      </div>
+
+   </div>
+
+</section>
